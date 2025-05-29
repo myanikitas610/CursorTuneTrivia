ec7f250219deb6003f5eb4d559c5ec682d890ff3
Mya Nikitas <mnikitas@fordham.edu>
2025-05-29 15:09:24 -0400

    Initial Commit

---
+++ CursorTuneTrivia/CursorTuneTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursorTuneTriviaApp.swift
+//  CursorTuneTrivia
+//
+//  Created by Mya Nikitas on 5/29/25.
+//
+
+import SwiftUI
+
+@main
+struct CursorTuneTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CursorTuneTriviaTests/CursorTuneTriviaTests.swift
@@ -0,0 +1,17 @@
+//
+//  CursorTuneTriviaTests.swift
+//  CursorTuneTriviaTests
+//
+//  Created by Mya Nikitas on 5/29/25.
+//
+
+import Testing
+@testable import CursorTuneTrivia
+
+struct CursorTuneTriviaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
